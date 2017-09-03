981d6bcef00064d08443e7475215e7606438f878
SocialAvesDev2 <SocialAvesDev2>
2017-09-03 16:53:39 +0545

    link error fixed

--- laravel/resources/backend/backend/layouts/footer.blade.php
+++ laravel/resources/backend/backend/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 0.01
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="http://treetechsolution.com">treeTech</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2016 <a href="#">treeTech</a>.</strong> All rights
     reserved.
 </footer>
 
